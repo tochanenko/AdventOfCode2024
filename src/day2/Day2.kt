7ac8e179c950e1c6a62106d7b04187ad1200e04d
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-12-02 17:35:13 +0200

    Add second solution for Day 2

---
+++ src/day2/Day2.kt
@@ -0,0 +1,65 @@
+package day2
+
+import java.io.File
+import java.io.InputStream
+
+fun main(args: Array<String>) {
+    val input: InputStream = File("./src/Day2/input.txt").inputStream()
+    val lines = mutableListOf<String>()
+    var safeSequences = 0
+
+    input.bufferedReader().forEachLine { lines.add(it) }
+    println("Lines scanned: ${lines.size}")
+    lines.forEach { line ->
+        run {
+            val values = line.split(" ").map { it.toInt() }
+            val isSafe = checkSequence(values)
+            if (isSafe) safeSequences++
+            println("${if (isSafe) "SAFE" else "UNSAFE"}: $values")
+        }
+    }
+
+    println("Res: $safeSequences")
+}
+
+fun isPairOk(numbers: List<Int>, i: Int, isDescending: Boolean): Boolean {
+    if (isDescending) {
+        if (numbers[i - 1] - numbers[i] !in 1..3) return false
+    } else {
+        if (numbers[i] - numbers[i - 1] !in 1..3) return false
+    }
+    return true
+}
+
+fun isArrayOk(numbers: List<Int>): Boolean {
+    val isDescending = numbers[0] > numbers[1]
+    for (i in 1..<numbers.size) {
+        if (!isPairOk(numbers, i, isDescending)) {
+            return false
+        }
+    }
+    return true
+}
+
+fun checkSequence(numbers: List<Int>): Boolean {
+    if (numbers.size < 2) return true
+
+    // Test original sequence
+    if (isArrayOk(numbers)) {
+        println("OK Sequence1: $numbers")
+        return true
+    }
+
+    // Test sequences with allowed 1 error
+    for (j in numbers.indices) {
+        val shortNumbers = numbers.toMutableList()
+        shortNumbers.removeAt(j)
+
+        if (isArrayOk(shortNumbers)) {
+            println("OK Sequence: $shortNumbers")
+            return true
+        }
+    }
+
+    return false
+}
---
+++ test/day2/Day2KtTest.kt
@@ -0,0 +1,18 @@
+package day2
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day2KtTest {
+
+    @Test
+    fun checkSequence() {
+        assertTrue(checkSequence(listOf(7, 6, 4, 2, 1)))
+        assertFalse(checkSequence(listOf(1, 2, 7, 8, 9)))
+        assertFalse(checkSequence(listOf(9, 7, 6, 2, 1)))
+        assertTrue(checkSequence(listOf(1, 3, 2, 4, 5)))
+        assertTrue(checkSequence(listOf(8, 6, 4, 4, 1)))
+        assertTrue(checkSequence(listOf(1, 3, 6, 7, 9)))
+        assertFalse(checkSequence(listOf(92, 91, 92, 93, 99)))
+    }
+}
