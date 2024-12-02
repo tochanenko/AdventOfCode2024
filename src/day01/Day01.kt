8811e09a629f7813006e02853729625974ee4418
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-12-02 17:53:01 +0200

    Implement solution for Day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,10 +1,29 @@
 package day01
 
 import readLines
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val lines = readLines("./src/day01/input.txt")
-    lines.forEach { line ->
+    val orderedLists = getOrderedLists(lines)
+    println("Distances: ${getDistances(orderedLists[0], orderedLists[1])}")
+}
 
+fun getOrderedLists(lines: List<String>): List<List<Int>> {
+    val firstList = mutableListOf<Int>()
+    val secondList = mutableListOf<Int>()
+    lines.forEach{ line -> run {
+        val numbers = line.split("   ")
+        firstList.add(numbers[0].toInt())
+        secondList.add(numbers[1].toInt())
+    }}
+    return listOf(firstList.sorted(), secondList.sorted())
+}
+
+fun getDistances(santaList: List<Int>, elvesList: List<Int>): Int {
+    var distances = 0
+    for (i in santaList.indices) {
+        distances += abs(santaList[i] - elvesList[i])
     }
+    return distances
 }
