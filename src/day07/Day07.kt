52cb3fec5917657fb56bc3dbf2a04a09c519279b
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-12-07 15:08:52 +0200

    Implement solution for Day 7 [part 2]

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,19 +1,85 @@
 package day07
 
 import readLines
+import kotlin.math.pow
+import kotlin.random.Random
 
 const val DEBUG = false
 
 data class Equation(
     val result: Long,
-    val numbers: List<Long>
+    val numbers: List<Long>,
+    var isCorrect: Boolean = false
 )
 
-enum class Operation { ADD, MULTIPLY }
+enum class Operation { ADD, MULTIPLY, CONCATENATE }
 
 fun main(args: Array<String>) {
     val lines = readLines("./src/day07/input.txt")
     val equations = lines.map { line -> lineToEquation(line) }
+//    output(equations) // it was too slow
+    outputRandom(equations) // :P
+}
+
+// LOL it worked
+fun outputRandom(equations: List<Equation>) {
+    val calculations = 2
+
+    for (i in 0..calculations) {
+        for (j in equations.indices) {
+            if (!equations[j].isCorrect && checkEquationRandom(equations[j])) {
+                print("($j) ")
+                equations[j].isCorrect = true
+            }
+        }
+        println()
+        println("[ ${equations.count { it.isCorrect }} / ${equations.size} ]")
+        println()
+        println()
+    }
+
+    println("Final sum: ${equations.filter { it.isCorrect }.sumOf { it.result }}")
+}
+
+fun lineToEquation(line: String): Equation = Equation(
+    line.substring(0, line.indexOfFirst { it == ':' }).toLong(),
+    line.substring(line.indexOfFirst{ it == ':'} + 1).trim().split(' ').map(String::toLong)
+)
+
+fun checkEquationRandom(equation: Equation): Boolean {
+    val totalCalculations: Int = 3.0.pow((equation.numbers.size - 1).toDouble()).toInt() * 2
+
+    for (calculation in 0..totalCalculations) {
+        val operations = Array(equation.numbers.size - 1) { Operation.ADD }
+        for (i in operations.indices) {
+            val randomOperation  = Random.nextInt(3)
+            when (randomOperation) {
+                1 -> operations[i] = Operation.MULTIPLY
+                2 -> operations[i] = Operation.CONCATENATE
+            }
+        }
+        if (calculateEquation(equation, operations)) return true
+    }
+
+    return false
+}
+
+fun calculateEquation(equation: Equation, operations: Array<Operation>): Boolean {
+    var res = equation.numbers[0]
+    for (i in 1..<equation.numbers.size) {
+        when (operations[i - 1]) {
+            Operation.ADD -> res += equation.numbers[i]
+            Operation.MULTIPLY -> res *= equation.numbers[i]
+            Operation.CONCATENATE -> res = (res.toString() + equation.numbers[i].toString()).toLong()
+        }
+        if (res > equation.result) return false
+    }
+    return equation.result == res
+}
+
+// ============ OLD SLOW SOLUTION ============
+
+fun output(equations: List<Equation>) {
     var sum = 0L
     var result = 0L
     for (i in equations.indices) {
@@ -25,11 +91,6 @@ fun main(args: Array<String>) {
     println("Final sum: $sum")
 }
 
-fun lineToEquation(line: String): Equation = Equation(
-    line.substring(0, line.indexOfFirst { it == ':' }).toLong(),
-    line.substring(line.indexOfFirst{ it == ':'} + 1).trim().split(' ').map(String::toLong)
-)
-
 fun checkEquation(equation: Equation): Boolean {
     val operations = Array(equation.numbers.size - 1) { Operation.ADD }
     if (calculateEquation(equation, operations)) return true
@@ -40,31 +101,31 @@ fun checkNextOperation(equation: Equation, operations: Array<Operation>): Boolea
     printEquation(equation, operations)
     if (calculateEquation(equation, operations)) return true
     for (i in operations.indices) {
-        if (operations[i] != Operation.MULTIPLY) {
-            val testOperations = operations.copyOf()
-            testOperations[i] = Operation.MULTIPLY
-            if (checkNextOperation(equation, testOperations)) return true
+        if (operations[i] == Operation.ADD) {
+            // *
+            val testOperationsMultiplication = operations.copyOf()
+            testOperationsMultiplication[i] = Operation.MULTIPLY
+            if (checkNextOperation(equation, testOperationsMultiplication)) return true
+
+            // ||
+            val testOperationsConcatenation = operations.copyOf()
+            testOperationsConcatenation[i] = Operation.CONCATENATE
+            if (checkNextOperation(equation, testOperationsConcatenation)) return true
         }
     }
     return false
-}
-
-fun calculateEquation(equation: Equation, operations: Array<Operation>): Boolean {
-    var res = equation.numbers[0]
-    for (i in 1..<equation.numbers.size) {
-        when (operations[i - 1]) {
-            Operation.ADD -> res += equation.numbers[i]
-            Operation.MULTIPLY -> res *= equation.numbers[i]
-        }
-    }
-    return equation.result == res
 }
 
 fun printEquation(equation: Equation, operations: Array<Operation>) {
     if (!DEBUG) return
     print("[${if (calculateEquation(equation, operations)) "RIGHT" else "WRONG"}] ${equation.result}: ${equation.numbers[0]}")
     for (i in 1..<equation.numbers.size) {
-        print("${if (operations[i - 1] == Operation.ADD) " + " else " * "}${equation.numbers[i]}")
+        when (operations[i - 1]) {
+            Operation.ADD -> print(" + ")
+            Operation.MULTIPLY -> print(" * ")
+            Operation.CONCATENATE -> print(" || ")
+        }
+        print(equation.numbers[i])
     }
     println()
 }
