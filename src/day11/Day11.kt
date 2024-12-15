e842216879497ff70bbe62161582d667db64b672
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-12-15 02:08:14 +0200

    Implement solution for Day 11 [part 2]

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -5,22 +5,23 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
-    val lines = readLines("./src/day11/input_smol.txt")
-    println(processStones(lines[0].split(" ").map { it.toInt() }, 6))
+    val lines = readLines("./src/day11/input.txt")
+    println(processStones(lines[0].split(" ").map { it.toLong() }, 75))
 }
 
-fun processStone(stones: List<Int>, times: Int): Int {
-    fun blink(stones: List<Int>): List<Int> {
-        val blinked = mutableListOf<Int>()
+fun processStone(stones: List<Long>, times: Long): Long {
+    fun blink(stones: List<Long>): List<Long> {
+        val blinked = mutableListOf<Long>()
         stones.forEach {
-            val stoneSize = log10(it.toDouble()).toInt() + 1
+            val stoneSize = log10(it.toDouble()).toLong() + 1
             when {
-                it == 0 -> blinked.add(1)
-                stoneSize % 2 == 0 -> {
-                    val splitPoint = 10.0.pow((stoneSize / 2).toDouble()).toInt()
+                it == 0L -> blinked.add(1)
+                stoneSize % 2 == 0L -> {
+                    val splitPoint = 10.0.pow((stoneSize / 2).toDouble()).toLong()
                     blinked.add(it / splitPoint)
                     blinked.add(it % splitPoint)
                 }
+
                 else -> {
                     blinked.add(it * 2024)
                 }
@@ -34,13 +35,52 @@ fun processStone(stones: List<Int>, times: Int): Int {
     for (i in 1..times) {
         processedStones = blink(processedStones)
     }
-    return processedStones.size
+    return processedStones.size.toLong()
 }
 
-fun processStones(stones: List<Int>, times: Int): Long {
+// Pair of stoneNumber and stoneTimesLeft as a key to result that they give
+val calculationTable = HashMap<Pair<Long, Long>, Long>()
+
+fun processStoneRecursive(stone: Long, times: Long): Long {
+    val currentStone = Pair(stone, times)
+    if (calculationTable.containsKey(currentStone))
+        return calculationTable[currentStone]!!
+
+    if (times == 0L) {
+        calculationTable[currentStone] = 1
+        return 1
+    } else {
+        val stoneSize = log10(stone.toDouble()).toLong() + 1
+        when {
+            stone == 0L -> {
+                val numberOfStones = processStoneRecursive(1L, times - 1)
+                calculationTable[currentStone] = numberOfStones
+                return numberOfStones
+            }
+
+            stoneSize % 2 == 0L -> {
+                val splitPoint = 10.0.pow((stoneSize / 2).toDouble()).toLong()
+                val numberOfStones =
+                    processStoneRecursive(stone / splitPoint, times - 1) +
+                            processStoneRecursive(stone % splitPoint, times - 1)
+                calculationTable[currentStone] = numberOfStones
+                return numberOfStones
+            }
+
+            else -> {
+                val numberOfStones = processStoneRecursive(stone * 2024, times - 1)
+                calculationTable[currentStone] = numberOfStones
+                return numberOfStones
+            }
+        }
+    }
+}
+
+fun processStones(stones: List<Long>, times: Long): Long {
     var res = 0L
 
-    stones.forEach { res += processStone(listOf(it), times) }
+//    stones.forEach { res += processStone(listOf(it), times) }
+    stones.forEach { res += processStoneRecursive(it, times) }
 
     return res
 }
