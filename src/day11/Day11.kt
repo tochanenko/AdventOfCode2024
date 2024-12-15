f64d28c3a87372826efda507d74f3c048e3f1e55
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-12-15 14:53:50 +0200

    Implement solution for Day 11 [refactor]

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -6,9 +6,40 @@ import kotlin.math.pow
 
 fun main(args: Array<String>) {
     val lines = readLines("./src/day11/input.txt")
+
+    val startTime = System.currentTimeMillis()
     println(processStones(lines[0].split(" ").map { it.toLong() }, 75))
+    val finishTime = System.currentTimeMillis()
+    println("Calculated in ${finishTime - startTime} ms")
 }
 
+// Pair of stoneNumber and stoneTimesLeft as a key to result that they give
+val calculationTable = HashMap<Pair<Long, Long>, Long>()
+
+fun processStoneRecursive(stone: Long, times: Long): Long {
+    val currentStone = Pair(stone, times)
+    if (calculationTable.containsKey(currentStone))
+        return calculationTable[currentStone]!!
+
+    if (times == 0L) return 1L.also { calculationTable[currentStone] = it }
+
+    val stoneSize = log10(stone.toDouble()).toLong() + 1
+    return when {
+        stone == 0L -> processStoneRecursive(1L, times - 1)
+
+        stoneSize % 2 == 0L -> {
+            val splitPoint = 10.0.pow((stoneSize / 2).toDouble()).toLong()
+            return processStoneRecursive(stone / splitPoint, times - 1) +
+                        processStoneRecursive(stone % splitPoint, times - 1)
+        }
+
+        else -> processStoneRecursive(stone * 2024, times - 1)
+    }.also { calculationTable[currentStone] = it }
+}
+
+fun processStones(stones: List<Long>, times: Long) = stones.sumOf { processStoneRecursive(it, times) }
+
+// Slow solution
 fun processStone(stones: List<Long>, times: Long): Long {
     fun blink(stones: List<Long>): List<Long> {
         val blinked = mutableListOf<Long>()
@@ -36,51 +67,4 @@ fun processStone(stones: List<Long>, times: Long): Long {
         processedStones = blink(processedStones)
     }
     return processedStones.size.toLong()
-}
-
-// Pair of stoneNumber and stoneTimesLeft as a key to result that they give
-val calculationTable = HashMap<Pair<Long, Long>, Long>()
-
-fun processStoneRecursive(stone: Long, times: Long): Long {
-    val currentStone = Pair(stone, times)
-    if (calculationTable.containsKey(currentStone))
-        return calculationTable[currentStone]!!
-
-    if (times == 0L) {
-        calculationTable[currentStone] = 1
-        return 1
-    } else {
-        val stoneSize = log10(stone.toDouble()).toLong() + 1
-        when {
-            stone == 0L -> {
-                val numberOfStones = processStoneRecursive(1L, times - 1)
-                calculationTable[currentStone] = numberOfStones
-                return numberOfStones
-            }
-
-            stoneSize % 2 == 0L -> {
-                val splitPoint = 10.0.pow((stoneSize / 2).toDouble()).toLong()
-                val numberOfStones =
-                    processStoneRecursive(stone / splitPoint, times - 1) +
-                            processStoneRecursive(stone % splitPoint, times - 1)
-                calculationTable[currentStone] = numberOfStones
-                return numberOfStones
-            }
-
-            else -> {
-                val numberOfStones = processStoneRecursive(stone * 2024, times - 1)
-                calculationTable[currentStone] = numberOfStones
-                return numberOfStones
-            }
-        }
-    }
-}
-
-fun processStones(stones: List<Long>, times: Long): Long {
-    var res = 0L
-
-//    stones.forEach { res += processStone(listOf(it), times) }
-    stones.forEach { res += processStoneRecursive(it, times) }
-
-    return res
 }
